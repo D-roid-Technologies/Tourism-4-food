422be3b6dd55ab0cbd4df66459b93e7adc904788
DeveloperRichard1 <mail.ricx@gmail.com>
2024-10-30 17:54:09 +0100

    Completed the About Us page

---
+++ src/App/UI/components/lite__button/Button.tsx
@@ -0,0 +1,60 @@
+// Button.tsx
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Buton.css";
+
+interface ButtonProps {
+  label?: string;
+  href?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+  variant?: "default" | "blur";
+  children?: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  children,
+  href,
+  onClick,
+  type = "button",
+  disabled = false,
+  className = "",
+  style = {},
+  variant = "default",
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = (event: React.MouseEvent) => {
+    if (onClick) onClick();
+    if (href) {
+      event.preventDefault();
+      navigate(href);
+    }
+  };
+
+  const buttonClass = `button ${
+    variant === "blur" ? "button--blur" : ""
+  } ${className}`;
+
+  return href ? (
+    <a href={href} onClick={handleClick} className={buttonClass} style={style}>
+      {children || label}
+    </a>
+  ) : (
+    <button
+      type={type}
+      onClick={handleClick}
+      disabled={disabled}
+      className={buttonClass}
+      style={style}
+    >
+      {children || label}
+    </button>
+  );
+};
+
+export default Button;
--- src/App/Utils/constant/Assets.ts
+++ src/App/Utils/constant/Assets.ts
@@ -5,6 +5,11 @@ const Assets = {
   images: {
     companyLogo: require("../../Assets/Images/Tourism-logo.png"),
     threeDots: require("../../Assets/Images/dots.png"),
+    lightBuld: require("../../Assets/Images/Light bulb made with yellow ball of yarn on blue backdrop.png"),
+    pushPin: require("../../Assets/Images/Hand-drawn green pushpin illustration.png"),
+    aboutHistoryImage: require("../../Assets/Images/about__history.png"),
+    ceoDummy: require("../../Assets/Images/CEO.png"),
+    dooDummy: require("../../Assets/Images/DOO.png"),
   },
 };
 
