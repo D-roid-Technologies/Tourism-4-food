085cf59eb6394020c05b1a3b5be7dd9fcec36eab
developerekene <developerekene@gmail.com>
2024-10-31 13:29:12 +0000

    updates to dev

--- src/App/UI/Pages/home/Home.tsx
+++ src/App/UI/Pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../../components/navbar/NavBar";
-import Footer from "../../components/footer/Footer";
+import Navbar from "../../Components/navbar/NavBar";
+import Footer from "../../Components/footer/Footer";
 import "../home/Home.css";
 
 const Home: React.FunctionComponent = () => {
