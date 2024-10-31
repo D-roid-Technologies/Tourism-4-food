61cc3cd2c3d7848ed6b1464f5e6b43bab1d40896
stellanechi <stellanechi65@gmail.com>
2024-10-31 15:05:39 +0100

    fixing git conflict

--- src/App/UI/Pages/aboutus/AboutUs.tsx
+++ src/App/UI/Pages/aboutus/AboutUs.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "../../Components/navbar/NavBar";
-import Footer from "../../Components/footer/Footer";
+import Navbar from "../../components/navbar/NavBar";
+import Footer from "../../components/footer/Footer";
 import "../aboutus/AboutUs.css";
 import "../../Components/liteGrid@v1.0/lite-grid.css";
 import { Assets } from "../../../Utils/constant/Assets";
-import Button from "../../Components/lite__button/Button";
+import Button from "../../components/lite__button/Button";
 import { IoIosArrowForward } from "react-icons/io";
 
 //about section two content
@@ -172,7 +172,7 @@ const AboutUs: React.FC = () => {
                 }}
                 variant="blur"
               >
-                <IoIosArrowForward />{" "}
+                <IoIosArrowForward />
               </Button>
             </div>
           </div>
--- src/App/Utils/constant/Assets.ts
+++ src/App/Utils/constant/Assets.ts
@@ -5,19 +5,16 @@ const Assets = {
   images: {
     companyLogo: require("../../Assets/Images/Tourism-logo.png"),
     threeDots: require("../../Assets/Images/dots.png"),
-<<<<<<< HEAD
     homeHeroImage: require("../../Assets/Images/home-hero-image.png"),
     homeEatChallenge: require("../../Assets/Images/eat-challenge.png"),
     homeDonateImage: require("../../Assets/Images/home-donate-image.png"),
     LiberianFlag: require("../../Assets/Images/liberian-flag.png"),
     travelBag: require("../../Assets/Images/Cartoon style traveling concept with baggage.png"),
-=======
     lightBuld: require("../../Assets/Images/Light bulb made with yellow ball of yarn on blue backdrop.png"),
     pushPin: require("../../Assets/Images/Hand-drawn green pushpin illustration.png"),
     aboutHistoryImage: require("../../Assets/Images/about__history.png"),
     ceoDummy: require("../../Assets/Images/CEO.png"),
     dooDummy: require("../../Assets/Images/DOO.png"),
->>>>>>> 70de00b52bae128e5f4dc8cb5d3e3470b1676213
   },
 };
 
