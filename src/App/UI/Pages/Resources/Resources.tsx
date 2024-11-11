3e24b0a362e78c1ff1f0e65fd2e2b347db926432
stellanechi <stellanechi65@gmail.com>
2024-11-11 12:51:41 +0100

    Save local changes before merging

--- src/App/UI/Pages/Resources/Resources.tsx
+++ src/App/UI/Pages/Resources/Resources.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Navbar from "../../Components/navbar/NavBar";
-import Footer from "../../Components/footer/Footer";
+
 import "../../../../App.css";
 // importing programs css file for a unifrom banner style
 import "../programs/Programs.css";
 import "./Resources.css";
 import { Assets } from "../../../Utils/constant/Assets";
-import Button from "../../Components/button/Button";
+import Button from "../../components/button/Button";
+import Footer from "../../Components/footer/Footer";
+import Navbar from "../../Components/navbar/NavBar";
 // import { IoIosArrowForward } from "react-icons/io";
 
 interface research {
--- src/App/UI/Pages/aboutus/AboutUs.tsx
+++ src/App/UI/Pages/aboutus/AboutUs.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
-import Navbar from "../../Components/navbar/NavBar";
-import Footer from "../../Components/footer/Footer";
+
 import "../aboutus/AboutUs.css";
-import "../../Components/liteGrid@v1.0/lite-grid.css";
+import "../../components/liteGrid@v1.0/liteGrid.css";
 import { Assets } from "../../../Utils/constant/Assets";
-import Button from "../../Components/button/Button";
+import Button from "../../components/button/Button";
+
 // import { IoIosArrowForward } from "react-icons/io";
+import Footer from "../../Components/footer/Footer";
+import Navbar from "../../Components/navbar/NavBar";
 import { useLocation } from "react-router-dom"; // Import useLocation
 
 //about section two content
