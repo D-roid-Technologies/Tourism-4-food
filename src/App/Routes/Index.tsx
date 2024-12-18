8dce5a9fb82d946feb9c7efc0588fe134b6f553f
DeveloperRichard1 <mail.ricx@gmail.com>
2024-12-18 17:29:49 +0100

    worked on the responsiveness for the navbar across the app

--- src/App/Routes/Index.tsx
+++ src/App/Routes/Index.tsx
@@ -1,74 +1,58 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { RouterType } from "../Utils/Types";
-import Home from "../UI/Pages/home/Home";
-import AboutUs from "../UI/Pages/aboutus/AboutUs";
-import Login from "../UI/Pages/home/login/Login";
-import Signup from "../UI/Pages/home/signup/Signup";
-import Main from "../UI/Pages/main/Main";
-import Programs from "../UI/Pages/programs/Programs";
-import Campaigns from "../UI/Pages/campaigns/Campaigns";
-import GetInvolved from "../UI/Pages/getInvolved/GetInvolved";
-import Donate from "../UI/Pages/donate/Donate";
-import Bookings from "../UI/Pages/bookings/Bookings";
-import Contact from "../UI/Pages/contact/Contact";
-import Legal from "../UI/Pages/legal/Legal";
-import Volunteer from "../UI/Pages/volunteer/Volunteer";
-import Onboarding from "../UI/Pages/home/onboarding/Onboarding";
-import VerifyEmail from "../UI/Pages/verifyemail/VerifyEmail";
-import EnterCode from "../UI/Pages/verifyemail/enterCode/EnterCode";
-import CreateCode from "../UI/Pages/createCode/CreateCode";
-import Resources from "../UI/Pages/resources/Resources";
-import UserDashboard from "../UI/Pages/userdashboard/UserDashboard";
-import UserBook from "../UI/Pages/userBook/UserBook";
-
-const Index: React.FunctionComponent<RouterType> = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/donate" element={<Donate />} />
-        <Route path="/volunteer" element={<Volunteer />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/programs" element={<Programs />} />
-        <Route path="/campaigns" element={<Campaigns />} />
-        <Route path="/getinvolved" element={<GetInvolved />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/legal" element={<Legal />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/volunteer" element={<Volunteer />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/verifyemail" element={<VerifyEmail />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/entercode" element={<EnterCode />} />
-        <Route path="/createcode" element={<CreateCode />} />
-        <Route path="/userdashboard" element={<UserDashboard />} />
-        <Route path="/userbook" element={<UserBook />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default Index;
-
-// IGNORE - TESTING A LOADING FUNCTION
 // import React from "react";
-// import { BrowserRouter } from "react-router-dom";
-// import LoadingManager from "../UI/components/loadingManager/Loadingmanager";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { RouterType } from "../Utils/Types";
+// import Home from "../UI/Pages/home/Home";
+// import AboutUs from "../UI/Pages/aboutus/AboutUs";
+// import Login from "../UI/Pages/home/login/Login";
+// import Signup from "../UI/Pages/home/signup/Signup";
+// import Main from "../UI/Pages/main/Main";
+// import Programs from "../UI/Pages/programs/Programs";
+// import Campaigns from "../UI/Pages/campaigns/Campaigns";
+// import GetInvolved from "../UI/Pages/getInvolved/GetInvolved";
+// import Donate from "../UI/Pages/donate/Donate";
+// import Bookings from "../UI/Pages/bookings/Bookings";
+// import Contact from "../UI/Pages/contact/Contact";
+// import Legal from "../UI/Pages/legal/Legal";
+// import Volunteer from "../UI/Pages/volunteer/Volunteer";
+// import Onboarding from "../UI/Pages/home/onboarding/Onboarding";
+// import VerifyEmail from "../UI/Pages/verifyemail/VerifyEmail";
+// import EnterCode from "../UI/Pages/verifyemail/enterCode/EnterCode";
+// import CreateCode from "../UI/Pages/createCode/CreateCode";
+// import Resources from "../UI/Pages/resources/Resources";
+// import UserDashboard from "../UI/Pages/userdashboard/UserDashboard";
+// import UserBook from "../UI/Pages/userBook/UserBook";
 
-// const Index: React.FC = () => {
+// const Index: React.FunctionComponent<RouterType> = () => {
 //   return (
 //     <BrowserRouter>
-//       <LoadingManager />
+//       <Routes>
+//         <Route path="/" element={<Main />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/donate" element={<Donate />} />
+//         <Route path="/volunteer" element={<Volunteer />} />
+//         <Route path="/aboutus" element={<AboutUs />} />
+//         <Route path="/programs" element={<Programs />} />
+//         <Route path="/campaigns" element={<Campaigns />} />
+//         <Route path="/getinvolved" element={<GetInvolved />} />
+//         <Route path="/bookings" element={<Bookings />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route path="/legal" element={<Legal />} />
+//         <Route path="/resources" element={<Resources />} />
+//         <Route path="/volunteer" element={<Volunteer />} />
+//         <Route path="/onboarding" element={<Onboarding />} />
+//         <Route path="/verifyemail" element={<VerifyEmail />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/entercode" element={<EnterCode />} />
+//         <Route path="/createcode" element={<CreateCode />} />
+//         <Route path="/userdashboard" element={<UserDashboard />} />
+//         <Route path="/userbook" element={<UserBook />} />
+//       </Routes>
 //     </BrowserRouter>
 //   );
 // };
 
 // export default Index;
-<<<<<<< Updated upstream
 
 // IGNORE - TESTING A LOADING FUNCTION
 import React from "react";
@@ -84,5 +68,3 @@ const Index: React.FC = () => {
 };
 
 export default Index;
-=======
->>>>>>> Stashed changes
--- src/App/UI/components/loadingManager/Loadingmanager.tsx
+++ src/App/UI/components/loadingManager/Loadingmanager.tsx
@@ -18,8 +18,8 @@ import VerifyEmail from "../../Pages/verifyemail/VerifyEmail";
 import EnterCode from "../../Pages/verifyemail/enterCode/EnterCode";
 import Signup from "../../Pages/home/signup/Signup";
 import CreateCode from "../../Pages/createCode/CreateCode";
-import UserDashboard from "../../Pages/userDashboard/UserDashboard";
-import Resources from "../../Pages/Resources/Resources";
+import UserDashboard from "../../Pages/userdashboard/UserDashboard";
+import Resources from "../../Pages/resources/Resources";
 
 const LoadingManager: React.FC = () => {
   const [loading, setLoading] = useState(false);
