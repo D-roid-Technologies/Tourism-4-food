8c3f96f584b07e1bdbd488ccfb59d4fa7133d191
DeveloperRichard1 <mail.ricx@gmail.com>
2024-12-19 16:08:27 +0100

    worked on the home, resources and about us page mobile view

--- src/App/UI/Pages/Loading.tsx
+++ src/App/UI/Pages/Loading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../components/testLoading/Testloading.css";
 
 const Loading: React.FunctionComponent = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="loading__overlay">
+      <div className="loading__line"></div>
+    </div>
+  );
 };
 
 export default Loading;
